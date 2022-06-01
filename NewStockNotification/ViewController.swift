715193cc4d9c014108e9923b5c46db187575b2ac
tcha <hataeck@gmail.com>
2022-06-01 14:38:07 +0900

    Initial Commit

---
+++ NewStockNotification/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewStockNotification
+//
+//  Created by taechan on 2022/06/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
